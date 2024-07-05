444ddb8de2b69511bd44b2ef06a7cb59f1e3c913
dooryanagi <166465828+dooryanagi@users.noreply.github.com>
2024-07-05 06:10:25 +0000

    rest

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -3,6 +3,9 @@
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+  # 退会したユーザーが再びログインできないように制限
+  before_action :user_status, only: [:create]
+
   # GET /resource/sign_in
   # def new
   #   super
@@ -24,4 +27,29 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  # ログイン後、ログアウト後の行き先を指定
+  def after_sign_in_path_for(resource)
+	  root_path
+  end
+
+  def after_sign_out_path_for(resource)
+	  new_user_session_path
+  end
+
+  private
+
+    # 会員のログインに関わる情報を確認する
+    def user_status
+      # ログインフォームに入力されたemail情報をもとにuserを見つけ、その情報を変数に格納する
+	    user = User.find_by(email: params[:user][:email])
+      return if user.nil?
+      return unless user.valid_password?(params[:user][:password])
+      if user.is_active == true
+    		create
+　    else
+    		redirect_to new_user_registration_path
+      end
+    end
+
 end
