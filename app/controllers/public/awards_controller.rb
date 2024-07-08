b401558f040a2a4f1f36649ba694ae074fdd9d5d
dooryanagi <166465828+dooryanagi@users.noreply.github.com>
2024-07-08 06:05:32 +0000

    Fix&Add test specifications

--- app/controllers/public/awards_controller.rb
+++ app/controllers/public/awards_controller.rb
@@ -1,4 +1,8 @@
 class Public::AwardsController < ApplicationController
+  # 本人以外は編集、更新、削除ができない
+  # 削除はここに定義せずともできない（ページがないため）
+  before_action :is_matching_login_user, only: [:edit, :update, :destroy]
+
   def new
     @award = Award.new
   end
@@ -7,11 +11,19 @@ class Public::AwardsController < ApplicationController
     @award = Award.new(award_params)
     # form_withで送られてこないデータについて指定
     @award.user_id = current_user.id
-    @award.save
-    redirect_to award_path(@award)
+    # save出来れば詳細ページへ、できなければrenderでnewのまま
+    if @award.save
+      redirect_to award_path(@award)
+    else
+      render :new
+    end
   end
 
   def index
+    # 写真がある場合とない場合表示を上部と下部で分ける
+    # whereでそれぞれの場合毎の変数を定義
+    # ビューで分岐をする方がシンプル
+    # @awards_with_images = Award.joins(:award_image_attachment).distinct
     @awards = Award.all
   end
 
@@ -25,14 +37,22 @@ class Public::AwardsController < ApplicationController
 
   def update
     @award = Award.find(params[:id])
-    @award.update(award_params)
-    redirect_to award_path(@award)
+    # update出来れば詳細ページへ、できなければrenderでeditのまま
+    if @award.update(award_params)
+      redirect_to award_path(@award)
+    else
+      render :edit
+    end
   end
 
   def destroy
     @award = Award.find(params[:id])
-    @award.destroy
-    redirect_to awards_path
+    # destroy出来れば一覧ページへ、できなければrenderでshowのまま、は可能？
+    if @award.destroy
+      redirect_to awards_path
+    else
+      redirect_to awards_path
+    end
   end
 
   private
@@ -40,5 +60,14 @@ class Public::AwardsController < ApplicationController
   def award_params
     params.require(:award).permit(:user_id, :comment, :is_public, :award_image)
   end
+
+  def is_matching_login_user
+    award = Award.find(params[:id])
+    user_id = award.user_id
+    unless user_id == current_user.id
+      redirect_back fallback_location: awards_path
+    end
+  end
+
 
 end
