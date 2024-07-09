852ea81838ed144311826e404076dc43148ba82e
dooryanagi <166465828+dooryanagi@users.noreply.github.com>
2024-07-09 05:32:30 +0000

    rest

--- app/controllers/public/grand_prizes_controller.rb
+++ app/controllers/public/grand_prizes_controller.rb
@@ -1,8 +1,21 @@
 class Public::GrandPrizesController < ApplicationController
+
   def new
+    @grand_prize = GrandPrize.new
+  end
+
+  def create
+    @grand_prize = GrandPrize.new(grand_prize_params)
+    @grand_prize.owner_id = current_user.id
+    if @grand_prize.save
+      redirect_to grand_prizes_path
+    else
+      render :new
+    end
   end
 
   def index
+    @grand_prizes = GrandPrize.all
   end
 
   def show
@@ -10,4 +23,11 @@ class Public::GrandPrizesController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def grand_prize_params
+    params.require(:grand_prize).permit(:name, :introduction, :owner_id, :keyword_1, :keyword_2, :keyword_3, :grand_prize_image)
+  end
+
 end
