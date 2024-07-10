b48eebee6771aaf2db75fa63afa2d85c4e545706
dooryanagi <166465828+dooryanagi@users.noreply.github.com>
2024-07-10 01:22:55 +0000

    finish admin award

---
+++ app/controllers/admin/grand_prizes_controller.rb
@@ -0,0 +1,11 @@
+class Admin::GrandPrizesController < ApplicationController
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
---
+++ app/controllers/admin/waiting_events_controller.rb
@@ -0,0 +1,4 @@
+class Admin::WaitingEventsController < ApplicationController
+  def index
+  end
+end
